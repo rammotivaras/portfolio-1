a8f108b39698d5c8c358323ca4ae761ed8cd0b81
ramjimotivaras <rkmotivaras77@gmail.com>
2025-09-14 12:17:25 +0530

    add pro video

--- data/projects.ts
+++ data/projects.ts
@@ -4,7 +4,9 @@ export const Projects = [
     slug: "dwarka-cab",
     description:
       "Dwarka Cab is a taxi booking platform designed for easy and fast cab reservations in Dwarka. It features real-time booking, responsive UI, and secure payment integration with Stripe to provide a seamless travel booking experience.",
-    short: "https://cdn.jsdelivr.net/gh/milankatira/project-videos/dwarkacab.mp4", // apna demo ya image/video link daal dena
+    short: "https://cdn.jsdelivr.net/gh/rammotivaras/project-videos@main/rk Ram's Video - Sep 14, 2025-VEED (1).mp4",
+    
+    // apna demo ya image/video link daal dena
     features: [
       "Online taxi booking system",
       "Location tracking with maps",
@@ -31,7 +33,7 @@ export const Projects = [
     slug: "dwarka-bike-rentals",
     description:
       "Dwarka Bike Rentals is a two-wheeler rental platform built to help travelers easily rent bikes and scooters in Dwarka. It provides quick booking, rental plans, and responsive mobile-first design for tourists and locals alike.",
-    short: "https://cdn.jsdelivr.net/gh/milankatira/project-videos/dwarka-bikes.mp4", // apna demo ya image/video link daal dena
+    short: "https://cdn.jsdelivr.net/gh/rammotivaras/project-videos@main/Screen Recording - Sep 14, 2025-VEED.mp4", // apna demo ya image/video link daal dena
     features: [
       "Bike and scooter booking system",
       "Responsive mobile-first design",
@@ -57,7 +59,7 @@ export const Projects = [
     slug: "bottle-art-museum",
     description:
       "Bottle Art Museum India is a creative website showcasing unique bottle art collections and cultural artifacts. It features engaging galleries, smooth animations, and a modern, responsive design to highlight the museum’s offerings.",
-    short: "https://cdn.jsdelivr.net/gh/milankatira/project-videos/bottle-art.mp4", // apna demo ya image/video link daal dena
+    short: "https://cdn.jsdelivr.net/gh/rammotivaras/project-videos@main/rk%20Ram's%20Video%20-%20Sep%2014,%202025-VEED.mp4", // apna demo ya image/video link daal dena
     features: [
       "Beautiful bottle art gallery",
       "Interactive UI with smooth animations",
